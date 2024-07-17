24fa4f430a1ff9451661805487d902d1d2239a67
philip.weinmann <Philip.Weinmann@protonmail.com>
2024-07-17 22:16:02 +0200

    feat(src): add iris recon with knn

---
+++ src/basics/iris-recon.py
@@ -0,0 +1,52 @@
+"""Simple Iris recon exercise
+"""
+
+import numpy as np
+from sklearn.datasets import load_iris # type: ignore
+from sklearn.model_selection import train_test_split # type: ignore
+from sklearn.neighbors import KNeighborsClassifier # type: ignore
+
+iris_dataset = load_iris()
+
+def create_knn_model(
+        training_data: tuple, 
+        neighbors: int, 
+        sample: list) -> np.ndarray:
+    """Classify data with k next neighbour algorithm.
+
+    Args:
+        None.
+
+    Returns:
+        prediction: An ndarray containing.
+
+    Raises:
+        None.
+    """
+    knn = KNeighborsClassifier(n_neighbors=neighbors)
+    knn.fit(training_data[0], training_data[1])
+    X_new = np.array(sample)
+    prediction = knn.predict(X_new)
+
+    return prediction
+
+def main() -> None:
+    """Main function
+
+    Executes and orchestrates all functions within this module.
+    """
+    # describe_parameters()
+    ds = iris_dataset
+    data   = ds['data']
+    target = ds['target']
+    names  = ds['target_names']
+    X_train, X_test, y_train, y_test = train_test_split(data, target, 
+                                                        random_state=0)
+    prediction = create_knn_model((X_train, y_train), 1, X_test)
+    print("Prediction: {}".format(prediction))
+    print("Predicted name: {}".format(names[prediction]))
+    accuracy = np.mean(prediction == y_test)
+    print("Accuracy of model with KNN: {}".format(accuracy))
+
+if __name__ == '__main__':
+    main()
