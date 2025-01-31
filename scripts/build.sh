186c05a3a467faf168851e5844a3a737d77107ea
philip.weinmann <Philip.Weinmann@protonmail.com>
2025-01-31 23:27:12 +0100

    feat(src): attempt to mimic an OR gate

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,5 +5,7 @@
 set -ex
 
 gcc src/c/main.c -o bin/main -std=c11 -lm
+gcc src/c/n-tuple-input.c -o bin/n-tuple-inputs -std=c11 -lm
+gcc src/c/gates.c -o bin/gates -std=c11 -lm
 
-bin/main
+#bin/main
