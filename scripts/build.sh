95e54b2425f7dfea820dd13a7f5566ffd8c761a5
philip.weinmann <Philip.Weinmann@protonmail.com>
2025-01-31 20:59:52 +0100

    feat(*): create base node

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,4 +5,5 @@
 set -ex
 
 gcc src/c/main.c -o bin/main -std=c11 -lm
-#clang -Wall -Wextra -o main main.c
+
+bin/main
--- src/c/main.c
+++ src/c/main.c
@@ -1,17 +1,61 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 // BASE IDEA: I want to model something, but I do not have the model
 float train[][2] = {
-  {0, 0},
-  {1, 2},
-  {2, 4},
-  {3, 6},
-  {4, 8},
+    {0, 0},
+    {1, 2},
+    {2, 4},
+    {3, 6},
+    {4, 8},
 };
+
+#define train_count (sizeof(train)/sizeof(train[0]))
+
+float rand_float(void)
+{
+    return (float) rand() / (float) RAND_MAX;
+}
+
+float cost(float w)
+{
+    float result = 0.0f;
+    for (size_t i = 0; i < train_count; ++i)
+    {
+        float x = train[i][0];
+        float y = x*w;
+        float d = y - train[i][1];
+        result += d*d;
+    }
+
+    result /= train_count;
+    return result;
+}
 
 int main() 
 {
-  printf("Hello World!");
-  return 0;
+    // srand(time(0));
+    srand(69);
+    // y = x*w;
+    //float w = rand_float()*10.0f;
+    float w = 1.0f;
+  
+    
+    float eps = 1e-3;
+    float rate = 1e-3;
+  
+    printf("%f\n", cost(w));
+    for (size_t i = 0; i < 500; ++i) {
+        float dcost = (cost(w + eps) - cost(w))/eps;
+        w -= rate*dcost;
+        printf("cost = %f, w = %f\n", cost(w), w);    
+    }
+
+    printf("\n-------------------------------------\n");
+    printf("%f\n", w);
+
+        
+    return 0;
 }
 
